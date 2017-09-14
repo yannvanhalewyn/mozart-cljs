0127e5732638c4f618b06d1486e04e2c9b09278b
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2017-09-14 15:01:21 +0200

    Make a keyboard view component

--- src/daw/views.cljs
+++ src/daw/views.cljs
@@ -1,5 +1,22 @@
 (ns daw.views
   (:require [re-frame.core :as re-frame]))
 
+(defn keyboard []
+  [:ul.keyboard
+   [:li.key--white.key]
+   [:li.key--black.key]
+   [:li.key--white.key--white-center.key]
+   [:li.key--black.key]
+   [:li.key--white.key--white-right.key]
+
+   [:li.key--white.key]
+   [:li.key--black.key]
+   [:li.key--white.key--white-center.key]
+   [:li.key--black.key]
+   [:li.key--white.key--white-center.key]
+   [:li.key--black.key]
+   [:li.key--white.key--white-right.key]
+   [:li.key--white.key]])
+
 (defn main-panel []
-  [:div "Main panel"])
+  [keyboard])
